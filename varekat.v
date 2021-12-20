e9e733622f7ab70790b9955b3cf66cc514b89e6f
Louis Schmieder <dev@louis-schmieder.de>
2021-12-20 23:29:28 +0100

    fix fmt ignore

--- varekat.v
+++ varekat.v
@@ -76,8 +76,11 @@ fn main() {
 			1, 0), 1)
 	}
 	game.user_ptr = &game
-	game.world = g.create_world(name: 'test', ambient_strenght: 0.1, light_color: mathf.vec3<f32>(1,
-		1, 1))
+	game.world = g.create_world(
+		name: 'test'
+		ambient_strenght: 0.1
+		light_color: mathf.vec3<f32>(1, 1, 1)
+	)
 
 	game.game_init_fn(game.user_ptr)
 
@@ -148,7 +151,10 @@ fn (mut game Game) start_vulkan() ? {
 	)
 	game.instance.setup() ?
 
-	game.uniform_buffer = game.instance.create_uniform_buffer<UBO>(stage: .vk_shader_stage_vertex_bit, descriptor_type: .vk_descriptor_type_uniform_buffer)?
+	game.uniform_buffer = game.instance.create_uniform_buffer<UBO>(
+		stage: .vk_shader_stage_vertex_bit
+		descriptor_type: .vk_descriptor_type_uniform_buffer
+	) ?
 
 	swapchain_settings := vk.SwapchainSettings{
 		width: &game.width
@@ -165,16 +171,15 @@ fn (mut game Game) start_vulkan() ? {
 	mut swapchain := vk.create_swapchain(swapchain_settings, game.instance.to_swapchain_info())
 
 	pipeline_settings := vk.PipelineSettings{
-		primitive: u32(C.VK_PRIMITIVE_TOPOLOGY_TRIANGLE_LIST)  
+		primitive: u32(C.VK_PRIMITIVE_TOPOLOGY_TRIANGLE_LIST)
 		primitive_restart_enable: vulkan.vk_false
 		width: &game.width
 		height: &game.height
-
 		rasterizer: vk.RasterizerStateSettings{
 			depth_clamp_enabled: vulkan.vk_false
 			rasterizer_discard_enable: vulkan.vk_false
 			line_width: 1
-			fill_mode: u32(C.VK_POLYGON_MODE_LINE)           
+			fill_mode: u32(C.VK_POLYGON_MODE_LINE)
 			cull_mode: u32(C.VK_CULL_MODE_BACK_BIT)
 			front_face: u32(C.VK_FRONT_FACE_CLOCKWISE)
 			depth_bias_enable: vulkan.vk_false
@@ -182,14 +187,14 @@ fn (mut game Game) start_vulkan() ? {
 			depth_bias_clamp: 0
 			depth_bias_slope_factor: 0
 		}
-		blend: vk.ColorBlendSettings{	
+		blend: vk.ColorBlendSettings{
 			blend_enable: vulkan.vk_true
 			src_color_blend_factor: .vk_blend_factor_src_alpha
 			dst_color_blend_factor: .vk_blend_factor_one_minus_src_alpha
 			color_blend_op: .vk_blend_op_add
 			src_alpha_blend_factor: .vk_blend_factor_one
 			dst_alpha_blend_factor: .vk_blend_factor_zero
-			alpha_blend_op: .vk_blend_op_add  
+			alpha_blend_op: .vk_blend_op_add
 			color_write_mask: vulkan.vk_color_component_all
 		}
 		subpass: vk.SubpassDepSettings{
@@ -201,12 +206,9 @@ fn (mut game Game) start_vulkan() ? {
 			dst_access_mask: u32(C.VK_ACCESS_COLOR_ATTACHMENT_READ_BIT | C.VK_ACCESS_COLOR_ATTACHMENT_WRITE_BIT)
 			dependency_flags: 0
 		}
-
 		sample_count: u32(C.VK_SAMPLE_COUNT_1_BIT)
-
 		logic_op_enable: vulkan.vk_false
 		logic_op: .vk_logic_op_no_op
-
 		bind_point: .vk_pipeline_bind_point_graphics
 		uniform_buffers: [&game.uniform_buffer]
 	}
